d035c877bb18c63aed3be3c5a0c73a818ae06161
Niall Ryan <70298047+smallblue2@users.noreply.github.com>
2026-06-02 12:37:41 +0100

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,42 @@
+class Stack:
+
+    def __init__(self):
+        self.store = []
+
+    def push(self, elem):
+        self.store.append(elem)
+    
+    def pop(self):
+        store_len = len(self.store)
+        if store_len == 0:
+            return None
+        return self.store.pop(len(self.store) - 1)
+    
+    def peek(self):
+        store_len = len(self.store)
+        if store_len == 0:
+            return None
+        return self.store[len(self.store) - 1]
+    
+    def is_empty(self):
+        return len(self.store) == 0
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        op = {
+            "(": ")",
+            "{": "}",
+            "[": "]"
+        }
+        stack = Stack()
+        for b in s:
+            if b in op:
+                stack.push(op[b])
+            else:
+                o = stack.pop()
+                if b is None:
+                    return False
+                if o != b:
+                    return False
+        return stack.is_empty()
+
