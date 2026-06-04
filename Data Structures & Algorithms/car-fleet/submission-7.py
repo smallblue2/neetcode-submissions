8e5436b85959117f878d1e7752b1a21dfdee82ca
Niall Ryan <70298047+smallblue2@users.noreply.github.com>
2026-06-04 16:38:21 +0100

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        combined = [[p, s] for p,s in zip(position, speed)] #O(n)
+        combined.sort(key=lambda x: x[0]) #O(nlogn)
+        
+        stack = []
+        i = len(combined) - 1
+        while i >= 0:
+            atime = (target - combined[i][0]) / combined[i][1]
+            if not stack: # First Convoy
+                stack.append(atime)
+            elif atime > stack[-1]: # Faster, join the convoy
+                stack.append(atime)
+            i -= 1
+        
+        return len(stack)
