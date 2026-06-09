01412e792ec82f3e0958d84a3db39063b78786b8
Niall Ryan <70298047+smallblue2@users.noreply.github.com>
2026-06-09 17:27:09 +0100

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+            
+        base = ord('a')
+
+        # Build permutation fingerprint
+        fingerprint = [0] * 26
+        for c in s1:
+            fingerprint[ord(c) - base] += 1
+        fingerprint = tuple(fingerprint)
+
+        lookup = dict()
+        lookup[fingerprint] = True
+
+        window_length = len(s1)
+        current_fingerprint = [0] * 26
+        l = 0
+        r = 0
+        while r < window_length:
+            current_fingerprint[ord(s2[r]) - base] += 1
+            r += 1
+        
+        # Check fingerprint
+        check = tuple(current_fingerprint)
+        if lookup.get(check, False):
+            return True
+        
+        while r < len(s2):
+            # Move on
+            current_fingerprint[ord(s2[l]) - base] -= 1
+            current_fingerprint[ord(s2[r]) - base] += 1
+            l += 1
+            r += 1 
+            # Check fingerprint
+            check = tuple(current_fingerprint)
+            if lookup.get(check, False):
+                return True
+
+        return False
+
+        
+
